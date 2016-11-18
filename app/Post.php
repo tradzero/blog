4556235ca7dc4b9ab92e91f2e6f62a0791c55046
林振宇 <admin@drakframe.com>
2016-11-18 16:48:07 +0800

    部分模型功能， 配置修改

--- app/Post.php
+++ app/Post.php
@@ -10,4 +10,9 @@ class Post extends Model
     {
         return $this->belongsToMany('App\Tag');
     }
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -9,21 +9,16 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
-        'name', 'email', 'password',
+        'nickname', 'username', 'sex', 'password', 'mail'
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
 }
