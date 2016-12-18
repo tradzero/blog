f7304b804e1fc6667d38e16be7fd363be5d83e63
tradzero <admin@drakframe.com>
2016-12-18 12:12:55 +0800

    bootstrap-markdown

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,16 @@
+@extends('admin.master')
+
+@section('title')撰写新文章@endsection
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <!-- main -->
+            <div class="col-md-9">
+                <input class="form-control" type="text" placeholder="在此输入标题">
+            </div>
+            <!-- toolbar -->
+            <div class="col-md-3">
+            </div>
+        </div>
+    </div>
+@endsection
