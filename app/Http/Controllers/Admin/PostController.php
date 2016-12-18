4515efcd8b292ec5ee8c2543f4bc70309ff9504b
tradzero <admin@drakframe.com>
2016-12-18 10:48:07 +0800

    移除谷歌字体， 添加消息提示模块

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PostController extends Controller
+{
+    public function create()
+    {
+        // todo return create view
+    }
+
+}
