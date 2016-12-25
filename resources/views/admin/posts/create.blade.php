36af4dba3eeb97e12f10c4d9fcbef318ef9f8432
tradzero <admin@drakframe.com>
2016-12-25 12:16:34 +0800

    后台-新增文章界面优化

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -5,8 +5,8 @@
     <div class="container-fluid">
         <div class="row">
             <!-- main -->
-            <div class="col-md-9">
-                <form action="/admin/posts" method="POST">
+            <form action="/admin/posts" method="POST">
+                <div class="col-md-9">
                     {{ csrf_field() }}
                     <div class="form-group">
                         @if($errors->has('title'))
@@ -31,11 +31,29 @@
                             <textarea name="content" class="form-control" id="myEditor"></textarea>
                         </div>
                     </div>
-                </form>
-            </div>
-            <!-- toolbar -->
-            <div class="col-md-3">
-            </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">发表</button>
+                    </div>
+                </div>
+                <!-- toolbar -->
+                <div class="col-md-3">
+                    <div class="box">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">选项</h3>
+                        </div>
+                        <div class="box-body">
+                            <div class="form-group">
+                                <label class="form-control" for="visible-select">选择文章可见性</label>
+                                <select class="form-control" name="visible" id="visible-select">
+                                    <option value="0" selected>对所有人可见</option>
+                                    <option value="1">对认证用户可见</option>
+                                    <option value="2">隐藏(对自己可见)</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 @endsection
