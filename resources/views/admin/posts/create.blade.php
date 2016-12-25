78e7c7f91968b308942bbfa4eec3aefe02a05a51
tradzero <admin@drakframe.com>
2016-12-25 11:24:49 +0800

    测试通过，注意 debugger与引入的markdown有冲突

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -6,14 +6,32 @@
         <div class="row">
             <!-- main -->
             <div class="col-md-9">
-                <div class="form-group">
-                    <input class="form-control" type="text" placeholder="在此输入标题">
-                </div>
-                <div class="form-group">
-                    <div class="editor">
-                        <textarea id='myEditor'></textarea>
+                <form action="/admin/posts" method="POST">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        @if($errors->has('title'))
+                        <div class="alert alert-danger alert-dismissible">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                            <h4><i class="icon fa fa-ban"></i> 提示!</h4>
+                            {{ $errors->first('title') }}
+                        </div>
+                        @endif
+                        <input class="form-control" name="title" type="text" placeholder="在此输入标题">
                     </div>
-                </div>
+
+                    <div class="form-group">
+                        <div class="editor">
+                            @if($errors->has('content'))
+                            <div class="alert alert-danger alert-dismissible">
+                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                                <h4><i class="icon fa fa-ban"></i> 提示!</h4>
+                                {{ $errors->first('content') }}
+                            </div>
+                            @endif
+                            <textarea name="content" class="form-control" id="myEditor"></textarea>
+                        </div>
+                    </div>
+                </form>
             </div>
             <!-- toolbar -->
             <div class="col-md-3">
