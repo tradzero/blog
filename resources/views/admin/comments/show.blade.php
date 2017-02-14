de5941fb4f74b379c194230b77676ed466e2ee6a
林振宇 <admin@drakframe.com>
2017-02-14 16:56:26 +0800

    评论

--- resources/views/admin/comments/show.blade.php
+++ resources/views/admin/comments/show.blade.php
@@ -13,16 +13,51 @@
                 </div>
                 <div class="box-body">
                     <h4>评论内容</h4>
-                    <p>{{ $comment->comment }}</p>
+                    <blockquote>{{ $comment->comment }}</blockquote>
                     <h4>相关文章</h4>
                     <div class="attachment-block clearfix">
                         <a href="/post/{{ $comment->post->id }}">{{ $comment->post->title }}</a>
                     </div>
+                    <h4>当前状态</h4>
+                    <p>
+                        是否显示：<span class="label {{ $comment->is_deleted ? 'label-danger' : 'label-success' }}">{{ $comment->is_deleted ? '隐藏':'显示' }}</span>
+                         共有{{ $comment->like }}人点赞, {{ $comment->unlike }}人点踩
+                    </p>
                 </div>
                 <div class="box-footer">
-                    
+                    <div class="clearfix list-inline">
+                        <div class="pull-left">
+                            <h4>操作</h4>
+                        </div>
+                        <div class="pull-right">
+                            @if($comment->is_deleted)
+                                <button onclick="setVisibility('pass', {{ $comment->id }})" class="btn btn-success">通过</button>
+                            @else
+                                <button onclick="setVisibility('deny', {{ $comment->id }})" class="btn btn-danger">隐藏</button>
+                            @endif
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+@endsection
+
+@section('script')
+    <script src="//cdn.bootcss.com/axios/0.15.3/axios.min.js"></script>
+    <script>
+        function setVisibility(attribute, id)
+        {
+            var url = '/admin/comments/' + id + '/' + attribute;
+            axios.post(url, null, {
+                header: {
+                    'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                }
+            }).then(function(data) {
+                location.reload();
+            }).catch(function (error) {
+                console.log(error);
+            })
+        }
+    </script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -15,22 +15,25 @@ Route::get('/', 'Www\PostController@index');
 Route::get('/post/{id}', 'Www\PostController@show');
 Auth::routes();
 Route::get('/home', 'HomeController@index');
-Route::group(['middleware' => ['auth']], function() {
+Route::group(['middleware' => ['auth']], function () {
     Route::post('/comment/{id}', 'Www\CommentController@store');
 });
 Route::get('/tag/{id}', 'Www\TagController@show');
 Route::patch('/post/{id}/like', 'Www\PostController@like');
 Route::patch('/comment/{id}/like', 'Www\CommentController@like');
 
-Route::group(['prefix' => 'admin', 'middleware' => ['auth'], 'namespace' => 'Admin'], function() {
+Route::group(['prefix' => 'admin', 'middleware' => ['auth'], 'namespace' => 'Admin'], function () {
     Route::get('/', 'HomeController@index');
     Route::patch('posts/{id}/recovery', 'PostController@recovery')->name('posts.recovery');
     Route::resource('posts', 'PostController');
     Route::resource('tags', 'TagController');
+    
+    Route::post('comments/{id}/deny', 'CommentController@deny')->name('comments.deny');
+    Route::post('comments/{id}/pass', 'CommentController@pass')->name('comments.pass');
     Route::resource('comments', 'CommentController');
     Route::post('/upload', 'UploadController@qiniuUpload');
 });
 
-Route::group(['prefix' => 'api', 'middleware' => ['auth', 'admin'], 'namespace' => 'Api'], function() {
+Route::group(['prefix' => 'api', 'middleware' => ['auth', 'admin'], 'namespace' => 'Api'], function () {
     Route::resource('tag', 'TagController');
 });
