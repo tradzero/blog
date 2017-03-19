596a3d0b293edbe27ad18fe62dac191ac5f9fc47
tradzero <admin@drakframe.com>
2017-03-19 22:54:19 +0800

    修改错误提示组件

--- resources/views/admin/component/alert-danger.blade.php
+++ resources/views/admin/component/alert-danger.blade.php
@@ -1,5 +1,12 @@
 <div class="alert alert-danger alert-dismissible">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
     <h4><i class="icon fa fa-ban"></i> 提示!</h4>
-    {{ $errors }}
+    @if(count($errors) > 1)
+        @foreach($errors->all() as $error)
+            {{ $error }}
+            <br>
+        @endforeach
+    @else
+        {{ $errors }}
+    @endif
 </div>
