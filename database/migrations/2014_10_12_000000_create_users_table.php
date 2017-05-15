980357dd23adf95c6c135ab6fe7600033a0f4c55
tradzero <admin@drakframe.com>
2017-05-15 21:58:32 +0800

    修改为191

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('nickname', 255)->comment('昵称');
-            $table->string('username', 255)->unique()->comment('用户名');
-            $table->string('password', 255)->comment('密码');
+            $table->string('nickname', 191)->comment('昵称');
+            $table->string('username', 191)->unique()->comment('用户名');
+            $table->string('password', 191)->comment('密码');
             $table->boolean('sex')->default(0)->comment('性别');
-            $table->string('mail', 255)->comment('邮箱');
+            $table->string('mail', 191)->comment('邮箱');
             $table->tinyInteger('role')->default(2)->comment('用户权限 0 admin, 1 授权用户, 2 游客');
             $table->boolean('is_banned')->default(0)->comment('是否被禁用');
             $table->rememberToken();
