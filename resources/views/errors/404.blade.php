373ffa07758fc6f4b1fe91f190df1ad3a17d3568
林振宇 <admin@drakframe.com>
2017-07-10 10:47:21 +0800

    404page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Whoops! 页面不见了</title>
+
+    <style>
+        @import url('../../../css/Source Sans Pro.css');
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: -apple-system, system-ui, 'Source Sans Pro', "Microsoft YaHei", sans-serif;
+        }
+        div.infomation {
+            position: absolute;
+            top: 20px;
+            width: 100%;
+            text-align: center;
+            z-index: 2;
+        }
+        h2 {
+            font-weight: normal;
+        }
+    </style>
+</head>
+<body>
+    <div class="infomation">
+        <h1>404 Not Found</h1>
+        <h2>抱歉，看来你找错页面了</h2>
+        <p>
+            <a href="{{ url('/') }}">返回首页</a>
+            或者
+            <a href="mailto:admin@drakframe.com">联系我</a>
+        </p>
+    </div>
+</body>
+</html>
