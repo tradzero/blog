aeb2755f8d22fb8a10d9f4d16c517650027d715a
林振宇 <admin@drakframe.com>
2017-07-27 18:02:23 +0800

    更新tag post 多对多关联关系

--- app/Post.php
+++ app/Post.php
@@ -9,7 +9,7 @@ class Post extends Model
 {
     public function tags()
     {
-        return $this->belongsToMany('App\Tag');
+        return $this->belongsToMany('App\Tag', 'post_tag', 'post_id', 'tag_id');
     }
 
     public function comments()
--- app/PostTag.php
+++ app/PostTag.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class PostTag extends Model
 {
-    //
+    protected $incrementing = false;
+    protected $table = 'post_tag';
 }
