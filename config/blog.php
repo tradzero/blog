0a82b959207baf1ef5b734044129839f2f2eb342
林振宇 <admin@drakframe.com>
2017-07-31 16:25:31 +0800

    信息定制化

--- config/blog.php
+++ config/blog.php
@@ -1,6 +1,32 @@
 <?php
 
 return [
+    /**
+     * 博客站点名
+     */
+    'name' => config('app.name'),
+
+    /**
+     * 博客次标题
+     */
+    'second_title' => 'recoding, learning',
+
+    /**
+     * 博客header部分链接部分 建议不要超过5个
+     */
+    'urls' => [
+        'weibo'   => 'http://www.weibo.com/u/2681234077/',
+        'e-mail'  => 'mailto:admin@drakframe.com',
+        'laravel' => 'https://laravel.com/',
+    ],
+    
+    /**
+     * 博客footer部分链接部分
+     */
+    'footer_urls' => [
+        'WordPress主站' => '//www.drakframe.com/',
+    ],
+
     // 首页显示预览字数
     'preview_length' => '500',
     // 文章一页展示多少条
